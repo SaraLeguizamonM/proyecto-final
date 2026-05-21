db6ab82f4bb3930662d999219fc0a2fcb1aed487
andresojoo <adso1705@gmail.com>
2026-05-21 15:34:59 -0500

    CeldaMina y CeldaSegura V1

---
+++ CeldaMina.java
@@ -0,0 +1,34 @@
+// Tipo de celda que representa una celda con mina.
+// Extiende de celda, porque una celda con mina, es de por sí, una celda
+public class CeldaMina extends Celda {
+
+    // Constructor que crea la celda mina y le asigna su ubicación
+    // en cordendas (número de fila y columna) en el tablero
+    public CeldaMina(int fila, int columna) {
+        super(fila, columna);
+    }
+
+    // Si el juego terminó y perdimos, este método ayuda a saber si esta celda con mina explotó
+    public boolean esExplotada() {
+        return descubierta;
+    }
+
+    // El @override indica que se está reescribiendo el método abstracto de la clase madre (Celda)
+    // Se reescribe para decirle a esta clase hija que simbolos mostrar según el caso
+    @Override
+    public char getSimbolo() {
+
+        if (bandera) {
+            // Si la celda tiene bandera, muestra la 'P', simbolo parecido a la forma de una bandera
+            return 'P';
+        }
+
+        if (descubierta) {
+            // Si la mina explotó, es decir, que la celda con mina se descubrió,
+            // se muestra un asterisco que simboliza a la mina
+            return '*';
+        }
+
+        return ' '; // Si está oculta, se muestra vacía para no delatarse en el tablero
+    }
+}
---
+++ CeldaSegura.java
@@ -0,0 +1,67 @@
+// Tipo de celda que representa a las celdas normales que muestran
+// los números de la cantidad de minas alrededor
+public class CeldaSegura extends Celda {
+    private int minasCercanas; // Guarda la cantidad de minas alrededor de la celda (valor de 0 a 8)
+
+    // Constructor que se encarga de pasar las coordenadas a la clase madre Celda
+    public CeldaSegura(int fila, int columna) {
+        super(fila, columna);
+
+        // La cantidad de minas cercanas inicia en cero hasta que el tablero
+        // calcule la cantidad de minas alrededor de la celda
+        this.minasCercanas = 0;
+    }
+
+    // Método para get obtener el valor de las minas cercanas,
+    // y método set modificar el contador de minas vecinas
+    public int getMinasCercanas() { return minasCercanas; }
+    
+    public void setMinasCercanas(int minas) {
+        if (minas >= 0 && minas <= 8) {
+            this.minasCercanas = minas;
+        }
+    }
+
+    // El @override indica que se está reescribiendo el método abstracto de la clase madre (Celda)
+    // Se reescribe para decirle a esta clase hija que simbolos mostrar según el caso
+    @Override
+    public char getSimbolo() {
+
+        // Si tiene la celda tiene bandera pero el tablero la descubrió al perder,
+        // es decir, que la bandera estaba mal puesta, muestra una X indicando
+        // que en la celda había una bandera mal puesta
+        if (bandera && descubierta) {
+            return 'X'; // Muestra la X de bandera incorrecta
+        }
+        
+        if (bandera) {
+            // Si la celda tiene bandera y sigue oculta durante la partida normal, es decir, sin perder
+            return 'P';
+        }
+        
+        // Si la casilla ya fue descubierta por el jugador o la cascada,
+        // y el valor de minas cercanas de esa celda es 0
+        if (descubierta) {
+            if (minasCercanas == 0) {
+                return '0'; // Muestra el cero explícito para las zonas vacías donde no hay mina
+            }
+            
+            // Mediente un switch (que intercambie de enteros a char), el número correspondiente
+            // a minas cercanas se covierte de un entero a un caracter para que el método abstracto
+            // pueda funcionar
+            switch (minasCercanas) {
+                case 1: return '1';
+                case 2: return '2';
+                case 3: return '3';
+                case 4: return '4';
+                case 5: return '5';
+                case 6: return '6';
+                case 7: return '7';
+                case 8: return '8';
+                default: return '0';
+            }
+        }
+        
+        return ' '; // Si la celda está oculta y no tiene bandera, se muestra vacía
+    }
+}
